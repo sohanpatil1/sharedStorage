d3671c2e64b36329fa2cc5a3d38143b3651c6934
sohanpatil <sohanpatil@gmail.com>
2024-03-18 19:51:57 -0700

    WIP

--- testing/sub.py
+++ testing/sub.py
@@ -8,7 +8,8 @@ def on_connect(client, userdata, flags, rc, properties):
         print(f"Connection failed with code {rc}")
 
 def on_message(client, userdata, msg):
-    print(f"Received message: {msg.payload.decode()}")
+    print(f"Received message: {msg.payload.decode()} at topic {msg.topic}")
+    client.subscribe("receiver2")
 
 client = mqtt.Client(callback_api_version=mqtt.CallbackAPIVersion.VERSION2)
 client.on_connect = on_connect
